c42da3efd5225018aa2cf785fd583a672e3aa040
AlanSamBB <alansamps@gmail.com>
2023-04-04 13:37:20 -0700

    Initial Commit

---
+++ CF.QuadMath-00/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CF.QuadMath-00
+//
+//  Created by Alan Sampson on 04/04/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
